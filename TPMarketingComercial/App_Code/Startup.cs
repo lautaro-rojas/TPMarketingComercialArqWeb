b9b9617a56ed43ec26153d7dc3482211c76abb9b
Navegador <Navegador@090L7PC03-73741>
2026-04-07 22:21:07 -0300

    Agregar archivos de proyecto.

---
+++ TPMarketingComercial/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TPMarketingComercial.Startup))]
+namespace TPMarketingComercial
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
